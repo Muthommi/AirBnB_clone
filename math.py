451caed2f3c4c38560b4a3ce110246c7f088fd20
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-18 22:53:05 -0700

    Be pycodestyle compliant

--- math.py
+++ math.py
@@ -2,21 +2,22 @@
 
 import cmd
 
+
 class Math(cmd.Cmd):
-    """simple command line processor."""
+    """Simple command line processor."""
 
     prompt = '(hbnb)'
 
     def do_multiply(self, s):
-        """multiply two numbers provided as arguments."""
+        """Multiply two numbers provided as arguments."""
         args = s.split()
         if len(args) != 2:
-            print("invalid calculation")
+            print("Invalid calculation")
             return
         try:
             args = [int(arg) for arg in args]
         except ValueError:
-            print("arguments should be numbers")
+            print("Arguments should be numbers")
             return
         print(args[0] * args[1])
 
@@ -24,9 +25,11 @@ class Math(cmd.Cmd):
         """Handle end of file (EOF)command to exit."""
         print("Exiting....")
         return True
+
     def do_quit(self, line):
         """Quit command to exit the program."""
         return True
+
 
 if __name__ == '__main__':
     import sys
