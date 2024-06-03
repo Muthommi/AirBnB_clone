03b7fa96c4bd0ff9ce2ffbd03f44201d9fb053a5
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-06-02 22:14:26 -0700

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines the HBNBCommand class for the command interpreter.
+"""
+
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter class for HBNB"""
     prompt = '(hbnb)'
 
     def do_quit(self, arg):
-        """Quit command that exits the program"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
